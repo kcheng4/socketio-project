a59f04ebeb8887e25cf8086d529a4c6da1e56839
Kenny <kcheng4@villanova.edu>
2018-12-14 18:03:21 -0500

    Adding listeners and emitters

--- server/server.js
+++ server/server.js
@@ -15,10 +15,23 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New User connected');
+
+  socket.emit('newMessage',{
+    from:"Javier",
+    text:"slam it down someee",
+    createdAt:123
+  });
+
+  socket.on('createMessage',(data) => {
+    console.log(data);
+  });
+
   socket.on('disconnect',() => {
     console.log('Disconnected from server');
   });
 });
+
+
 server.listen(port,() => {
   console.log(`Server is up and running on port ${port}`);
 });
